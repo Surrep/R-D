d7775f632f7dcfaecb3e9959f683cabdd923ea94
Truman Purnell <truman.purnell@gmail.com>
2018-03-18 14:16:01 -0400

    adds file-watcher to research folder

--- logger.py
+++ logger.py
@@ -1,11 +1,16 @@
-import os, time
-path_to_watch = "./texts"
-before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-while 1:
-  time.sleep (10)
-  after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-  added = [f for f in after if not f in before]
-  removed = [f for f in before if not f in after]
-  if added: print("Added: ", ", ".join (added))
-  if removed: print("Removed: ", ", ".join (removed))
-  before = after
+import os
+import time
+import sys
+
+file_name, path_to_watch = sys.argv
+before = dict([(f, None) for f in os.listdir(path_to_watch)])
+
+while True:
+    time.sleep(10)
+    after = dict([(f, None) for f in os.listdir(path_to_watch)])
+    added = [f for f in after if not f in before]
+
+    if added:
+        print(added)
+
+    before = after
