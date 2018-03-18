b1d86f749c96b50465bd2a0e911dd3419a01f28e
Truman Purnell <truman.purnell@gmail.com>
2018-03-18 00:14:33 -0400

    adds directional arrows to traversal

--- form/flood.js
+++ form/flood.js
@@ -4,31 +4,20 @@ function flood(region, r, c, dir = "") {
 
     if (region.data[id] && !region.visited.has(id)) {
 
-        region.getCell(id).style.backgroundColor = region.color
+        region.getCell(id).style.backgroundImage = `url(/Users/tru/Desktop/photos/${dir}.png)`
         region.visited.add(id)
 
-        let result = false
         console.log(r, c, dir)
-        result = flood(region, r - 1, c - 1, "\\")
-        if (result) return true
-        result = flood(region, r - 1, c, "|")
-        if (result) return true
-        result = flood(region, r - 1, c + 1, "/")
-        if (result) return true
+        result = flood(region, r - 1, c - 1, "diag-left")
+        result = flood(region, r - 1, c, "up-down")
+        result = flood(region, r - 1, c + 1, "diag-right")
 
-        result = flood(region, r, c - 1, "-")
-        if (result) return true
-        result = flood(region, r, c + 1, "-")
-        if (result) return true
+        result = flood(region, r, c - 1, "left-right")
+        result = flood(region, r, c + 1, "left-right")
 
-        result = flood(region, r + 1, c - 1, "/")
-        if (result) return true
-        result = flood(region, r + 1, c, "|")
-        if (result) return true
-        result = flood(region, r + 1, c + 1, "\\")
-        if (result) return true
-
-        return true
+        result = flood(region, r + 1, c - 1, "diag-right")
+        result = flood(region, r + 1, c, "up-down")
+        result = flood(region, r + 1, c + 1, "diag-left")
 
     }
 
