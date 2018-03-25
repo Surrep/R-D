91834189a5c39ce63d647bd635a776b8f3041eb8
Truman Purnell <truman.purnell@gmail.com>
2018-03-24 22:46:55 -0400

    trims spot -> edge pipeline to two methods

--- edges/scan.py
+++ edges/scan.py
@@ -28,67 +28,48 @@ out = np.zeros((rows, cols, 3))
 spots = set()
 edges = defaultdict(EdgeBox)
 
-# setup for edge connection
-angle_bins = np.linspace(pi / 2, -pi / 2, 12)
-rows, cols = np.meshgrid(receptive_field, receptive_field)
-angles = np.arctan(cols / rows)
-
 print('loaded')
 
 
-def find_spots(r, c):
+def find_spots():
     receptive_field = range(-1, 2)  # 3x3
-    color = image[r, c]
 
-    for r_off in receptive_field:
-        for c_off in receptive_field:
-            detect(color, r + r_off, c + c_off)
+    for r in range(1, rows - 1):
+        for c in range(1, cols - 1):
+            color = image[r, c]
+
+            for r_off in receptive_field:
+                for c_off in receptive_field:
+                    if image[r + r_off, c + c_off] != color:
+                        spots.add((r, c))
+                        # out[r, c] = [255, 255, 255]
 
 
-def detect(color, r, c):
-    if image[r, c] != color:
-        spots.add((r, c))
-        out[r, c] = [255, 255, 255]
-
-
-def connect(spot):
-    r, c = spot
-
+def find_edges():
     receptive_field = np.arange(-3, 4)  # 7x7
-    field = image[r + receptive_field, c + receptive_field]
+    rows, cols = np.meshgrid(receptive_field, receptive_field)
+    angles = np.arctan(cols / rows)
+    angle_bins = np.linspace(1.5, -1.5, 12)
 
-    np.digitize(angles[field > 0], angle_bins)
+    while len(spots):
+        r, c = spots.pop()
+        field = image[r + receptive_field, c + receptive_field]
+
+        np.digitize(angles[field > 0], angle_bins) % 12
 
 
-def get_angle(r, c):
-    return pi / 2 if not c else atan(r / c)
-
-
-print('spotting')
+print('--------spotting----------')
 start = time.time()
-
-for r in range(1, rows - 1):
-    for c in range(1, cols - 1):
-        find_spots(r, c)
-
+find_spots()
 end = time.time()
 print(end - start)
 print(len(spots))
 
-# print('edging')
-# start = time.time()
-
-# while len(spots):
-#     connect(spots.pop())
-
-# end = time.time()
-# print(end - start)
-# print(len(set(edges.values())))
-
-# for edge in set(edges.values()):
-#     color = np.random.randint(0, 256, (3))
-#     for spot in edge.spots:
-#         out[spot] = color
+print('--------edging-----------')
+start = time.time()
+find_edges()
+end = time.time()
+print(end - start)
 
 
 imsave('/Users/tru/Desktop/out22.jpg', out)
