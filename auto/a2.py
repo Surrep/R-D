4d0ce52d79ec432358dcfbc8b26f58fe3da3c074
Truman Purnell <truman.purnell@gmail.com>
2018-04-16 17:41:58 -0400

    fits sine curve

--- auto/a2.py
+++ auto/a2.py
@@ -1,36 +1,35 @@
 from scipy.io.wavfile import read, write
+import matplotlib.pyplot as plt
 import numpy as np
 
 sounds = '/Users/tru/Desktop/english/'
 sample_rate, data = read(sounds + 'four.wav')
 
-data = data.reshape(1, -1)
-channels, samples = data.shape
+# Auto encoder
+x = np.sin(np.arange(20).reshape(1, -1))
+y = x
 
 # D_in is input dimension
 # H is hidden dimension
 # D_out is output dimension
-D_in, H, D_out = samples, 100, samples
-learning_rate = 0.001
-
-# Auto encoder
-x = data
-y = data
+D_in, H, D_out = x.shape[1], 19, x.shape[1]
+learning_rate = 1e-3
 
 # Randomly initialize weights
 w1 = np.random.randn(D_in, H)
 w2 = np.random.randn(H, D_out)
 
-for itr in range(1000):
+ans = None
+for itr in range(100):
     # forward pass
     z1 = x.dot(w1)
     a1 = np.maximum(z1, 0)  # relu
     z2 = a1.dot(w2)
 
+    ans = z2
     # compute cost
     cost = np.square(z2 - y).sum() / 2
-    if not itr % 25:
-        print(cost)
+    print(cost)
 
     # backward pass
     dy = z2 - y
@@ -40,5 +39,23 @@ for itr in range(1000):
     dz1[z1 < 0] = 0
     dw1 = x.T.dot(dz1)
 
+    # print(np.linalg.norm(x), 'x')
+    # print(np.linalg.norm(w1), 'w1')
+    # print(np.linalg.norm(z1), 'z1')
+    # print(np.linalg.norm(a1), 'a1')
+
+    # print(y, 'y')
+    # print(dy, 'dy')
+    # print(dw2[0, :2])
+    # print(da1[0, :2])
+    # print(dz1[0, :2])
+    # print(dw1[0, :2])
+    # print('------------------')
+
     w1 -= learning_rate * dw1
     w2 -= learning_rate * dw2
+
+
+plt.plot(x[0])
+plt.plot(ans[0])
+plt.show()
--- auto/a3.py
+++ auto/a3.py
@@ -1,40 +1,42 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.io.wavfile import read, write
 
-# N is batch size(sample size); D_in is input dimension;
+sounds = '/Users/tru/Desktop/english/'
+sample_rate, data = read(sounds + 'four.wav')
+
+data = data.reshape(1, -1)
+channels, samples = data.shape
+
+# N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 4, 2, 30, 1
+D_in, H, D_out = samples, 300, samples
 
-# Create random input and output data
-x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([[0], [1], [1], [0]])
+x = data
+y = data
 
 # Randomly initialize weights
 w1 = np.random.randn(D_in, H)
 w2 = np.random.randn(H, D_out)
 
-learning_rate = 0.002
-loss_col = []
-for t in range(1):
+learning_rate = 1e-6
+for t in range(500):
     # Forward pass: compute predicted y
     h = x.dot(w1)
-    h_relu = np.maximum(h, 0)  # using ReLU as activate function
+    h_relu = np.maximum(h, 0)
     y_pred = h_relu.dot(w2)
 
-    
+    # Compute and print loss
+    loss = np.square(y_pred - y).sum() / 2
+    print(t, loss)
+
     # Backprop to compute gradients of w1 and w2 with respect to loss
-    grad_y_pred = 2.0 * (y_pred - y)  # the last layer's error
-
-    print(h_relu.shape)
-    print(grad_y_pred.shape)
-
+    grad_y_pred = y_pred - y
     grad_w2 = h_relu.T.dot(grad_y_pred)
-    grad_h_relu = grad_y_pred.dot(w2.T)  # the second laye's error
+    grad_h_relu = grad_y_pred.dot(w2.T)
     grad_h = grad_h_relu.copy()
-    grad_h[h < 0] = 0  # the derivate of ReLU
+    grad_h[h < 0] = 0
     grad_w1 = x.T.dot(grad_h)
 
     # Update weights
     w1 -= learning_rate * grad_w1
     w2 -= learning_rate * grad_w2
-
