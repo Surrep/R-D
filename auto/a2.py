9a87d1a951706f5f5ba7badd7d4abcebb510fb50
Truman Purnell <truman.purnell@gmail.com>
2018-04-16 14:39:23 -0400

    adds auto-encoder v2 and 3

--- auto/a2.py
+++ auto/a2.py
@@ -4,12 +4,41 @@ import numpy as np
 sounds = '/Users/tru/Desktop/english/'
 sample_rate, data = read(sounds + 'four.wav')
 
-print(*data.shape)
+data = data.reshape(1, -1)
+channels, samples = data.shape
 
-w0 = np.random.randn(*data.shape, 100)
-a0 = data.dot(w0)
+# D_in is input dimension
+# H is hidden dimension
+# D_out is output dimension
+D_in, H, D_out = samples, 100, samples
+learning_rate = 0.001
 
-w1 = np.random.randn(100, *data.shape)
-a1 = a0.dot(w1)
+# Auto encoder
+x = data
+y = data
 
-print(((a1 - data) ** 2).sum())
+# Randomly initialize weights
+w1 = np.random.randn(D_in, H)
+w2 = np.random.randn(H, D_out)
+
+for itr in range(1000):
+    # forward pass
+    z1 = x.dot(w1)
+    a1 = np.maximum(z1, 0)  # relu
+    z2 = a1.dot(w2)
+
+    # compute cost
+    cost = np.square(z2 - y).sum() / 2
+    if not itr % 25:
+        print(cost)
+
+    # backward pass
+    dy = z2 - y
+    dw2 = a1.T.dot(dy)
+    da1 = dy.dot(w2.T)
+    dz1 = da1.copy()
+    dz1[z1 < 0] = 0
+    dw1 = x.T.dot(dz1)
+
+    w1 -= learning_rate * dw1
+    w2 -= learning_rate * dw2
