b74f9825af16772dcbd02f2e6bd8657b2e06634f
Truman Purnell <truman.purnell@gmail.com>
2018-04-16 23:52:32 -0400

    adds grad check

--- auto/a2.py
+++ auto/a2.py
@@ -6,30 +6,28 @@ sounds = '/Users/tru/Desktop/english/'
 sample_rate, data = read(sounds + 'four.wav')
 
 # Auto encoder
-x = np.sin(np.arange(20).reshape(1, -1))
+x = np.sin(np.arange(300).reshape(1, -1))
 y = x
 
-# D_in is input dimension
-# H is hidden dimension
-# D_out is output dimension
-D_in, H, D_out = x.shape[1], 19, x.shape[1]
-learning_rate = 1e-3
+# D_in is input dimension; H are hidden dimensions; D_out is output dimension
+D_in, H1, H2 D_out = x.shape[1], 100, 100, x.shape[1]
+learning_rate = 9e-6
 
 # Randomly initialize weights
-w1 = np.random.randn(D_in, H)
-w2 = np.random.randn(H, D_out)
+w1 = np.random.randn(D_in, H1)
+w2 = np.random.randn(H1, H2)
+w3 = np.random.randn(H2, D_out)
 
-ans = None
-for itr in range(100):
+for itr in range(7500):
     # forward pass
     z1 = x.dot(w1)
     a1 = np.maximum(z1, 0)  # relu
     z2 = a1.dot(w2)
+    a2 = np.maximum(z2, 0)  # relu
+    z3 = a2.dot(w3)
 
-    ans = z2
     # compute cost
-    cost = np.square(z2 - y).sum() / 2
-    print(cost)
+    cost = np.square(z3 - y).sum() / 2
 
     # backward pass
     dy = z2 - y
@@ -38,19 +36,6 @@ for itr in range(100):
     dz1 = da1.copy()
     dz1[z1 < 0] = 0
     dw1 = x.T.dot(dz1)
-
-    # print(np.linalg.norm(x), 'x')
-    # print(np.linalg.norm(w1), 'w1')
-    # print(np.linalg.norm(z1), 'z1')
-    # print(np.linalg.norm(a1), 'a1')
-
-    # print(y, 'y')
-    # print(dy, 'dy')
-    # print(dw2[0, :2])
-    # print(da1[0, :2])
-    # print(dz1[0, :2])
-    # print(dw1[0, :2])
-    # print('------------------')
 
     w1 -= learning_rate * dw1
     w2 -= learning_rate * dw2
