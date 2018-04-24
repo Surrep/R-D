b58280fd3c92e628c6c2b690a5eca937629aa6ef
Truman Purnell <truman.purnell@gmail.com>
2018-04-24 10:34:40 -0400

    adds padding code to end of sample

--- auto/auto_chunks.py
+++ auto/auto_chunks.py
@@ -9,23 +9,37 @@ print(sample_rate, data.shape)
 sequence_len = 10
 learning_rate = 1e-1
 
-X = np.sin(880 * np.arange(50)).reshape(1, -1)
+X = np.sin(880 * np.arange(2000)).reshape(1, -1)
 WI = np.random.randn(sequence_len, sequence_len)
+
+
+def forward_layer(prev, weights):
+    return prev.dot(weights)
+
+
+def compute_cost(yhat, y):
+    return ((yhat - y) ** 2 / 2).sum()
+
+
+def get_samples(data, t, sequence_len):
+    samples = data[:, t:t + sequence_len]
+    num_samples = len(samples[0])
+    padding = np.repeat(0, sequence_len - num_samples)
+
+    return np.append(samples, padding).reshape(1, -1)
+
 
 for t in range(len(X[0])):
     cost = 2  # inf
-    print('-----------------------------------', t)
-    while cost > 1e-8:
-        xt = X[:, t:t + sequence_len]
-        if xt.shape[1] != sequence_len:
-            break
+    # print('-----------------------------------', t)
+    while cost > 1e-5:
+        xt = get_samples(X, t, sequence_len)
+        zt = forward_layer(xt, WI)
+        cost = compute_cost(zt, xt)
 
-        zt = xt.dot(WI)
-
-        cost = ((zt - xt) ** 2 / 2).sum()
-        print(cost)
+        # print(cost)
 
         dy = zt - xt
         dWI = xt.T.dot(dy)
-
         WI -= learning_rate * dWI
+
