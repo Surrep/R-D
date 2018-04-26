abb829b7b7275eb03c382d5ea7b64c0823af5735
Truman Purnell <truman.purnell@gmail.com>
2018-04-26 13:31:51 -0400

    removes predict and generate methods

--- auto/auto_chunks.py
+++ auto/auto_chunks.py
@@ -13,80 +13,65 @@ def load_sound(path):
 
 class AutoRNN:
 
-    def __init__(self, learning_rate, sequence_len, look_ahead,
+    def __init__(self, learning_rate, seq_len, look_ahead,
                  stride, lucidity):
         self.lucidity = lucidity
         self.learning_rate = learning_rate
-        self.sequence_len = sequence_len
+        self.seq_len = seq_len
         self.look_ahead = look_ahead
         self.stride = stride
-        self.W1 = np.random.randn(sequence_len, sequence_len) * 0.01
 
-    def predict(self, X):
-        X = X.copy()
-        out = np.zeros_like(X)
-        for t in range(0, len(X[0]), self.stride):
-            xt = self.get_samples(X, t)
-            if xt is None:
-                break
-            zt = xt.dot(self.W1)
-            out[:, t:t + self.sequence_len] = zt
+        self.W1 = np.random.randn(seq_len, look_ahead) * 0.01
 
-        return out
-
-    def generate(self, seed, time_steps):
-        out = np.zeros((1, time_steps * self.sequence_len))
-
-        for t in range(0, time_steps, self.stride):
-            print(seed.round(3))
-            seed = seed.dot(self.W1)
-            print(seed.round(3))
-            out[:, t:t + self.sequence_len] = seed
-            print('--------------------------')
-
-        return out
-
-    def get_samples(self, data, t):
-        samples = data[:, t:t + self.sequence_len]
-        return None if len(samples[0]) < self.sequence_len else samples
+    def get_samples(self, data, t, num_samples):
+        return None if len(data[0]) < (t + num_samples) else data[:, t:t + num_samples]
 
     def compute_cost(self, yhat, y):
         return ((yhat - y) ** 2 / 2).sum()
 
     def fit(self, X, y, epsilon=1e-5, cap=500, verbose=False):
-        X = X.copy()
+        X = X.copy()  # copy data so as not to mutate original
+
+        # loop through entire sequence
         for t in range(0, len(X[0]), self.stride):
-            cost = math.inf
-            end_sequence = False
-            attempts_exhausted = False
+
+            xt = self.get_samples(X, t, self.seq_len)
+            yt = self.get_samples(y, t + self.seq_len, self.look_ahead)
+
+            if xt is None or yt is None:  # we have reached end of sequence
+                return
+
+            cost = math.inf  # cost is inf before training
             count = 0  # attempts to minimize loss
+            attempts_exhausted = False  # true if we take too long to minimize error
 
-            while cost > epsilon and not attempts_exhausted and not end_sequence:
-                xt = self.get_samples(X, t)
-                yt = self.get_samples(y, t + self.look_ahead)
+            # attempt to minimize distance between predictions and true sample
+            while cost > epsilon and not attempts_exhausted:
+                # get predictions
+                zt = xt.dot(self.W1)
 
-                end_sequence = xt is None or yt is None
+                # compute cost based on predictions
+                cost = self.compute_cost(zt, yt)
 
-                if not end_sequence:
-                    zt = xt.dot(self.W1)
-                    cost = self.compute_cost(zt, yt)
-                    dy = zt - yt
-                    dW1 = xt.T.dot(dy)
+                # propogate loss back to weights
+                dy = zt - yt
+                dW1 = xt.T.dot(dy)
 
-                    self.W1 -= self.learning_rate * dW1
+                # update weights based on loss
+                self.W1 -= self.learning_rate * dW1
 
-                    count += 1
-                    attempts_exhausted = count > cap
+                # check for terminating conditions
+                count += 1
+                attempts_exhausted = count > cap
 
-            if not end_sequence:
-                p_dist = [self.lucidity, 1 - self.lucidity]
-                choices = [False, True]
-                mask = np.random.choice(a=choices, size=zt.shape, p=p_dist)
+            p_dist = [self.lucidity, 1 - self.lucidity]
+            choices = [False, True]
+            mask = np.random.choice(a=choices, size=zt.shape, p=p_dist)
 
-                start = t + self.look_ahead
-                end = start + self.sequence_len
-                if (end - start) == self.sequence_len:
-                    X[:, start:end] = np.where(mask, zt, xt)
+            start = t + self.seq_len
+            end = start + self.look_ahead
+            if (end - start) == self.seq_len:
+                X[:, start:end] = np.where(mask, zt, xt)
 
             if verbose and not t % 1e3:
                 print(t, count, cost)
@@ -98,7 +83,7 @@ sample_rate, data = read(sounds + 'calc.wav')
 X = (data.reshape(1, -1) / np.max(data))[:, 55000:57000]
 y = X
 
-arnn = AutoRNN(learning_rate=0.1, sequence_len=10,
+arnn = AutoRNN(learning_rate=0.1, seq_len=10,
                look_ahead=10, stride=10, lucidity=0)
 
 errors = arnn.fit(X, y, epsilon=1e-13, cap=1000, verbose=True)
