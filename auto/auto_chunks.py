d221008de91b8ad50b466dda42795e7cf389c0aa
Truman Purnell <truman.purnell@gmail.com>
2018-04-26 13:09:15 -0400

    adds lucidity parameter

--- auto/auto_chunks.py
+++ auto/auto_chunks.py
@@ -1,6 +1,7 @@
 from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 def load_sound(path):
@@ -12,15 +13,19 @@ def load_sound(path):
 
 class AutoRNN:
 
-    def __init__(self, learning_rate, sequence_len):
+    def __init__(self, learning_rate, sequence_len, look_ahead,
+                 stride, lucidity):
+        self.lucidity = lucidity
         self.learning_rate = learning_rate
         self.sequence_len = sequence_len
-        self.W1 = np.random.randn(sequence_len, sequence_len)
+        self.look_ahead = look_ahead
+        self.stride = stride
+        self.W1 = np.random.randn(sequence_len, sequence_len) * 0.01
 
     def predict(self, X):
+        X = X.copy()
         out = np.zeros_like(X)
-
-        for t in range(len(X[0])):
+        for t in range(0, len(X[0]), self.stride):
             xt = self.get_samples(X, t)
             if xt is None:
                 break
@@ -29,34 +34,73 @@ class AutoRNN:
 
         return out
 
-    def get_samples(self, X, t):
-        samples = X[:, t:t + self.sequence_len]
+    def generate(self, seed, time_steps):
+        out = np.zeros((1, time_steps * self.sequence_len))
+
+        for t in range(0, time_steps, self.stride):
+            print(seed.round(3))
+            seed = seed.dot(self.W1)
+            print(seed.round(3))
+            out[:, t:t + self.sequence_len] = seed
+            print('--------------------------')
+
+        return out
+
+    def get_samples(self, data, t):
+        samples = data[:, t:t + self.sequence_len]
         return None if len(samples[0]) < self.sequence_len else samples
 
     def compute_cost(self, yhat, y):
         return ((yhat - y) ** 2 / 2).sum()
 
-    def fit(self, X, verbose=False):
-        errors = []
-        for t in range(len(X[0])):
-            cost = 2  # inf
+    def fit(self, X, y, epsilon=1e-5, cap=500, verbose=False):
+        X = X.copy()
+        for t in range(0, len(X[0]), self.stride):
+            cost = math.inf
+            end_sequence = False
+            attempts_exhausted = False
+            count = 0  # attempts to minimize loss
 
-            if verbose:
-                print('-----------------------------------', t)
-
-            while cost > 1e-1:
+            while cost > epsilon and not attempts_exhausted and not end_sequence:
                 xt = self.get_samples(X, t)
-                if xt is None:
-                    break
-                zt = xt.dot(W1)
-                cost = compute_cost(zt, xt)
-                errors.append(cost)
+                yt = self.get_samples(y, t + self.look_ahead)
 
-                if verbose:
-                    print(cost)
+                end_sequence = xt is None or yt is None
 
-                dy = zt - xt
-                dW1 = xt.T.dot(dy)
-                W1 -= learning_rate * dW1
+                if not end_sequence:
+                    zt = xt.dot(self.W1)
+                    cost = self.compute_cost(zt, yt)
+                    dy = zt - yt
+                    dW1 = xt.T.dot(dy)
 
-        return np.array(errors)
+                    self.W1 -= self.learning_rate * dW1
+
+                    count += 1
+                    attempts_exhausted = count > cap
+
+            if not end_sequence:
+                p_dist = [self.lucidity, 1 - self.lucidity]
+                choices = [False, True]
+                mask = np.random.choice(a=choices, size=zt.shape, p=p_dist)
+
+                start = t + self.look_ahead
+                end = start + self.sequence_len
+                if (end - start) == self.sequence_len:
+                    X[:, start:end] = np.where(mask, zt, xt)
+
+            if verbose and not t % 1e3:
+                print(t, count, cost)
+
+
+sounds = '/Users/tru/Desktop/english/'
+sample_rate, data = read(sounds + 'calc.wav')
+
+X = (data.reshape(1, -1) / np.max(data))[:, 55000:57000]
+y = X
+
+arnn = AutoRNN(learning_rate=0.1, sequence_len=10,
+               look_ahead=10, stride=10, lucidity=0)
+
+errors = arnn.fit(X, y, epsilon=1e-13, cap=1000, verbose=True)
+
+out2 = arnn.generate(X[:, :10], 1900)
