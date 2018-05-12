9aaf065a88ffcc4c1ae74de6578640ee8e18344d
Truman Purnell <truman.purnell@gmail.com>
2018-05-11 21:31:51 -0700

    single layer forward images map to a similiar space

--- cnn/sand.py
+++ cnn/sand.py
@@ -1,6 +1,18 @@
 from sklearn.datasets import fetch_mldata
 import matplotlib.pyplot as plt
+import numpy as np
 
 mnist = fetch_mldata('MNIST original')
-plt.imshow(mnist.data[1].reshape(28, 28))
-plt.show()
+i0 = mnist.data[0].reshape(28, 28)
+i1 = mnist.data[1].reshape(28, 28)
+
+W1 = np.random.randn(28, 2)
+W2 = np.random.randn(28, 1)
+
+
+def forward(image):
+    return image.dot(W1).T.dot(W2)
+
+
+print(forward(i0))
+print(forward(i1))
