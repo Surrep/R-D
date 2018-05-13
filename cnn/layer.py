38ae2314d86fa779ae808268628ac0078d2a59fa
Truman Purnell <truman.purnell@gmail.com>
2018-05-12 22:59:07 -0700

    demonstrates the spread of each digit class

--- cnn/layer.py
+++ cnn/layer.py
@@ -1,26 +1,47 @@
-from scipy.misc import imread, imsave
+from sklearn.datasets import fetch_mldata
+from math import sin, pi, tan, cos, inf
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 import numpy as np
 
-photo_repo = '/Users/tru/Desktop/photos/'
+mnist = fetch_mldata('MNIST original')
 
-photo = imread(photo_repo + 'shu.jpg')
-rows, cols, channels = photo.shape
-
-maps = 1
-L1 = np.random.randn(5, 5, 3, maps)
-receptive_field = np.arange(-2, 3)
-
-out = np.zeros((rows, cols, maps))
-
+maps = 30
+rows = cols = 28
+filter_size = 3
 margin = 5
-stride = 1
+stride = 8
+samples = 1500
+dims = 3
 
-for r in range(margin, rows - margin, stride):
-    for c in range(margin, cols - margin, stride):
-        for i in range(maps):
-            field = np.ix_(receptive_field + r, receptive_field + c)
-            activation = (photo[field] * L1[:, :, :, i]).sum()
-            out[r, c, i] = activation
+L1 = np.random.rand(filter_size, filter_size, 1, maps)
+receptive_field = np.arange(-1, 2)
+out = np.zeros((samples, rows, cols, maps))
 
-for filt in range(maps):
-    imsave('/Users/tru/Desktop/out2.jpg'.format(filt), out[:, :, filt])
+
+rand_idxs = np.random.choice(np.arange(70000), size=samples, replace=False)
+
+
+imgs = mnist.data[rand_idxs]
+clrs = mnist.target[rand_idxs]
+
+for im, img in enumerate(imgs):
+    img = img.reshape(rows, cols)
+
+    for r in range(margin, rows - margin, stride):
+        for c in range(margin, cols - margin, stride):
+            for i in range(maps):
+                field = np.ix_(receptive_field + r, receptive_field + c)
+
+                activation = img[field] * L1[:, :, :, i]
+                out[im, r, c, i] = activation.sum()
+
+points = np.zeros((samples, dims))
+L2 = np.random.rand(rows * cols * maps, dims)
+for im, img in enumerate(out):
+    points[im] = img.reshape(1, rows * cols * maps).dot(L2)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=clrs)
+plt.show()
--- cnn/sand.py
+++ cnn/sand.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import fetch_mldata
-from math import sin, pi, tan, cos
+from math import sin, pi, tan, cos, inf
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,40 +8,33 @@ mnist = fetch_mldata('MNIST original')
 
 
 def forward(image, layers):
-    dim_layer, *rest = layers
-    al = image.dot(dim_layer).T
-
+    al = image
     for l in layers:
-        al = al.dot(l)
+        al = np.tanh(al.dot(l))
 
     return al
 
 
 def init_weights(dims):
     return [
-        np.random.randn(dims[i], dims[i + 1]) * 0.1
+        np.random.randn(dims[i], dims[i + 1]) * 0.4
         for i in range(len(dims) - 1)
     ]
 
 
-mid = [int(sin(l) * 50) for l in np.linspace(0.1, pi - 0.1, 10)]
-W = init_weights([28, 3] + mid + [1])
+samples = 1000
+dims = 3
+start = 40000
+points = np.zeros((samples, dims))
+W = np.random.rand(784, dims)
 
+print(np.unique(mnist.target[start:start + samples]))
 
-samples = 5000
-points = np.zeros((samples, 3))
-rand_idxs = np.random.choice(np.arange(70000), size=samples, replace=False)
-
-imgs = mnist.data[rand_idxs]
-clrs = mnist.target[rand_idxs]
-
-for i, img in enumerate(imgs):
-    img = img.reshape(28, 28)
-    points[i] = forward(img, W).reshape(3)
+for i, image in enumerate(mnist.data[start:start + samples]):
+    points[i] = image.dot(W)
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=clrs)
-
+ax.scatter(points[:, 0], points[:, 1], points[:, 2])
 plt.show()
