d68e101b2b5a29e955102a6db5c7db7f3fe97593
Truman Purnell <truman.purnell@gmail.com>
2018-05-12 23:23:12 -0700

    demystifies randn vs rand

--- cnn/sand.py
+++ cnn/sand.py
@@ -10,29 +10,31 @@ mnist = fetch_mldata('MNIST original')
 def forward(image, layers):
     al = image
     for l in layers:
-        al = np.tanh(al.dot(l))
+        al = al.dot(l)
 
     return al
 
 
 def init_weights(dims):
     return [
-        np.random.randn(dims[i], dims[i + 1]) * 0.4
+        np.random.randn(dims[i], dims[i + 1]) * 0.01
         for i in range(len(dims) - 1)
     ]
 
 
-samples = 1000
+samples = 4000
 dims = 3
-start = 40000
+start = 0
 points = np.zeros((samples, dims))
-W = np.random.rand(784, dims)
+layers = init_weights([784, dims])
 
 print(np.unique(mnist.target[start:start + samples]))
 
 for i, image in enumerate(mnist.data[start:start + samples]):
-    points[i] = image.dot(W)
+    points[i] = forward(image.reshape(1, -1), layers)
 
+
+points = np.random.randn(samples, dims)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
