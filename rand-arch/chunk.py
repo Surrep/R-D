c45a1e1c6a3314ec41831379b8b450168f680962
Truman Purnell <truman.purnell@gmail.com>
2018-05-16 20:49:58 -0700

    completes random chunking of input

--- rand-arch/chunk.py
+++ rand-arch/chunk.py
@@ -11,7 +11,33 @@ def index_list(data):
     indices = np.indices(data.shape)
     return np.vstack([
         indices[i].ravel() for i in range(len(indices))
-    ])
+    ]).T
 
 
-print(index_list(mnist.data[0].reshape(28, 28)))
+def rand_rect(max_area):
+    w = np.random.randint(max_area) + 1
+    h = int(max_area / w)
+
+    return (w, h)
+
+
+def chunk_input(data):
+    indices = index_list(data)
+    r_indxs = list(np.random.choice(
+        len(indices), size=len(indices), replace=False))
+
+    chunks = []
+    while r_indxs:
+        area = np.random.randint(len(r_indxs))
+        w, h = rand_rect(area + 1)
+
+        chunk = np.array([data[tuple(indices[r_indxs.pop()])]
+                          for _ in range(w * h)])
+
+        chunks.append(chunk.reshape(w, h))
+
+    return chunks
+
+
+a = np.random.randn(5, 5, 3)
+chunks = chunk_input(a)
