3a17435f3f739918db00eb2583fd3ad8a9476aa6
Truman Purnell <truman.purnell@gmail.com>
2018-05-16 22:20:41 -0700

    random arch layers created

--- rand-arch/chunk.py
+++ rand-arch/chunk.py
@@ -1,61 +1,46 @@
 from sklearn.datasets import fetch_mldata
-from math import sin, pi, tan, cos, inf
+from math import sin, pi, tan, cos, inf, pow
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 mnist = fetch_mldata('MNIST original')
-dims = 2
 
 
-def get_indices(data):
-    indices = np.indices(data.shape)
-    return np.vstack([
-        indices[i].ravel() for i in range(len(indices))
+def get_indices(shape):
+    index_grid = np.indices(shape)
+    indices = np.vstack([
+        index_grid[i].ravel() for i in range(len(index_grid))
     ]).T
 
-
-def random_layer(neurons):
-    w = np.random.randint(max_area) + 1
-    h = int(max_area / w)
-
-    return (w, h)
+    return (indices, len(indices))
 
 
-def generate_architecture(input_data, output_dim):
-    valid_indices = get_indices(input_data)
-    i_count = len(valid_indices)
-    rand_inds = list(np.random.choice(i_count, size=i_count, replace=False))
-
-    while rand_inds:  # while there is data left to group
-        neuron_count = np.random.randint(len(rand_inds))
-        layer = random_layer(size=neuron_count, depth=len(input_data.shape))
-
-        chunk = np.array([data[tuple(indices[r_indxs.pop()])]
-                          for _ in range(w * h)])
-
-        chunks.append(chunk.reshape(w, h))
+def random_layer(size, depth):
+    return tuple([
+        np.random.randint(pow(size, 1 / depth)) + 1
+        for _ in range(depth)
+    ])
 
 
-def init_weights(data_chunks, dims):
-    return [
-        np.random.randn(chunk.shape[0], dims) * 0.1
-        for chunk in data_chunks
-    ]
+def generate_architecture(in_shape, out_shape):
+    indices = np.prod(in_shape)
+
+    layers = []
+    while indices:  # while there is data left to group
+        neuron_count = np.random.randint(indices) + 1
+        layer = random_layer(size=neuron_count, depth=len(in_shape))
+        layers.append(layer)
+
+        indices -= np.prod(layer)
+
+    return layers
 
 
-def forward(data_chunks, weights):
-    return np.array([chunk.T.dot(W).sum() for (chunk, W) in zip(data_chunks, weights)])
+def feed(arch, data):
+    pass
 
 
-a = mnist.data[0]
-
-input_chunks = chunk_input(a.reshape(28, 28))
-weights = init_weights(input_chunks, dims=dims)
-out = forward(input_chunks, weights)
-
-final_layer = np.random.randn(len(out), dims)
-points = out.reshape(1, -1).dot(final_layer)
-
-plt.scatter(points[:, 0], points[:, 1], c=targets)
-plt.show()
+a = mnist.data[0].reshape(28, 28)
+arch = generate_architecture(in_shape=a.shape, out_shape=None)
+print(arch)
