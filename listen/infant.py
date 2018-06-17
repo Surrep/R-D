5aeff918a2690d7946d4c220305f85025bc7e9f0
Truman Purnell <truman.purnell@gmail.com>
2018-06-17 15:23:09 -0700

    adds incremental mean fn

--- listen/infant.py
+++ listen/infant.py
@@ -5,17 +5,22 @@ import numpy as np
 import sys
 
 sound_lib_path = '/Users/tru/Desktop/english/'
-# sample_rate, data = read(sounds + 'tru1.wav')
-# 28000 - 50000 -- Hello for calc.wav
 
 
-def create_time_fourier(sound, opts=(2 ** 14, 2 ** 5, 100)):
+def compute_mean_step(old_mean, new_sample, step):
+    return old_mean + (1 / step) * (new_sample - old_mean)
+
+
+def spectrogram(sound, opts=(2 ** 14, 2 ** 5, 5)):
     lapse, channels, skip = opts
     out = np.zeros((len(sound) // skip, lapse // channels))
+    brain = np.random.rand(lapse // channels) * 0.01
+    brain_file = open('/Users/tru/Desktop/drain.txt', 'w')
 
     for ts, t in enumerate(range(0, len(sound) - lapse, skip)):
         fft_at_t = np.fft.fft(sound[t:t + lapse], n=lapse // channels)
         out[ts] = np.absolute(fft_at_t)
+        print(brain.dot(out[ts]), file=brain_file)
 
     return out
 
@@ -28,7 +33,7 @@ def flatten(sound):
 
 
 sounds = [read(sound_lib_path + sound) for sound in sys.argv[1:]]
-spectra = [create_time_fourier(flatten(data[:150000])) for rate, data in sounds]
+spectra = [spectrogram(flatten(data[:450000])) for rate, data in sounds]
 
 for i, spectrum in enumerate(spectra):
-    imsave('/Users/tru/Desktop/spec{}.jpeg'.format(i), spectrum.T)
+    imsave('/Users/tru/Desktop/spec{}.jpeg'.format(i), spectrum)
