69bbab71b2a4aeb234f114aec12fdaddd87e671a
Truman Purnell <truman.purnell@gmail.com>
2019-05-16 00:00:47 -0700

    pads bounds for images in response to parity of dims

--- trace/fourier.py
+++ trace/fourier.py
@@ -12,11 +12,11 @@ class Resource:
         self.string = self.stub % self.attrs
 
 
-b = 8
-p = 1.0
+b = 0
+p = 1
 
-bin_res = Resource(('../data/bins/', '12', 'txt'))
-img_res = Resource(('../data/images/', 'mom', 'jpg'))
+bin_res = Resource(('../data/bins/', '2', 'txt'))
+img_res = Resource(('../data/images/', 'sb', 'jpg'))
 
 image = imread(img_res.string)
 bins = np.array(pd.read_csv(bin_res.string, header=None)[0])
@@ -26,16 +26,18 @@ image_bincounts = np.bincount(image_binned.reshape(-1))
 dense_region = np.argsort(image_bincounts)[b]
 image_density_map = image_binned == dense_region
 
-w, h = map(lambda d: int(d / 2), image_binned.shape)
-grid = np.meshgrid(range(-h, h), range(-w, w))
+w, h = map(lambda d: d // 2, image_binned.shape)
+grid = np.meshgrid(range(-h - 1, h + 1), range(-w - 1, w + 1))
 
 coords = np.vstack(map(lambda a: a[image_density_map], grid)).T.astype(float)
 coords[:, 0] /= h
-coords[:, 1] /= w
+coords[:, 1] /= -w
 
 path = coords[np.random.choice(a=[True, False],
                                size=coords.shape[0],
-                               p=[p, 1 - p])].view(complex)
+                               p=[p, 1 - p])].reshape(-1).view(complex)
 
-plt.plot(path.real, -path.imag, '.')
+cs = np.fft.fft(path)
+
+plt.plot(cs.real, cs.imag, '.')
 plt.show()
