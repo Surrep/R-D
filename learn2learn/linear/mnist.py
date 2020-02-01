9f607546eb52a786fd93ca1fd6c301db3d447c4d
trumanpurnell <truman.purnell@gmail.com>
2020-02-01 00:33:10 -0800

    swaps mnist stochastic for batch gradient descent

--- learn2learn/linear/mnist.py
+++ learn2learn/linear/mnist.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from mpl_toolkits.axes_grid1 import AxesGrid
-
-
 from mlxtend.data import loadlocal_mnist
 
 # Get training data:
@@ -10,22 +9,23 @@ X, y = loadlocal_mnist(
     images_path='/Users/trumanpurnell/Workspace/bblabs/deep-blob/learn2learn/data/train-images-idx3-ubyte',
     labels_path='/Users/trumanpurnell/Workspace/bblabs/deep-blob/learn2learn/data/train-labels-idx1-ubyte')
 
+# Preprocess
 X = X.astype(float) / 255
+y_hot = np.zeros((y.size, 10))
+y_hot[np.arange(y.size), y] = 1
 
 # Initialize the Weights: 10 Template Images which we will match against
-weights = np.random.randn(784, 10)
+weights = np.random.rand(784, 10) / 10
 
 # Train
-alpha = 0.01
-for i in range(100):
-    error = 0
-    for Xi, yi in zip(X[:1000], y[:1000]):
-        prediction = weights.T.dot(Xi)
-        prediction[yi] -= 1
-        error += np.linalg.norm(prediction)
-        gradient = Xi.reshape(-1, 1).dot(prediction.reshape(-1, 1).T)
+alpha = 0.0000001
+for i in range(1000):
+    predictions = X.dot(weights)
+    deltas = predictions - y_hot
+    error = np.linalg.norm(deltas)
+    gradient = X.T.dot(deltas)
 
-        weights -= alpha * gradient
+    weights -= alpha * gradient
 
     if not (i % 10):
         print(error)
