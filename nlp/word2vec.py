8f504ee658deda375ac26820891d9a0d2f50bb33
trumanpurnell <truman.purnell@gmail.com>
2020-02-13 01:25:51 -0800

    running backprop on word2vec without negative sampling or cross-entropy loss

--- nlp/word2vec.py
+++ nlp/word2vec.py
@@ -1,11 +1,18 @@
 import string
 import numpy as np
 
+# Load Data
 with open("./data/bible.txt") as fs:
     without_punctuation = str.maketrans('', '', string.punctuation)
 
     corpus = fs.read().lower().translate(without_punctuation).split()
-    vocab = {k: v for v, k in enumerate(corpus)}
+
+    vocab = {}
+    identity = 0
+    for word in corpus:
+        if word not in vocab:
+            vocab[word] = identity
+            identity += 1
 
 # Hyperparameters
 alpha = 0.0001
@@ -14,11 +21,11 @@ window_size = 3
 embedding_size = 40
 
 weights_0_1_dims = (len(vocab), embedding_size)
-weights_1_0_dims = (embedding_size, len(vocab))
+weights_1_2_dims = (embedding_size, len(vocab))
 
 # Weights
 weights_0_1 = np.random.randn(*weights_0_1_dims)
-weights_1_0 = np.random.randn(*weights_1_0_dims)
+weights_1_2 = np.random.randn(*weights_1_2_dims)
 
 # Training
 sample = corpus[:16]
@@ -26,4 +33,26 @@ sample = corpus[:16]
 for iteration in range(iterations):
     for si in range(window_size, len(sample) - window_size + 1):
         window = sample[si-window_size: si+window_size]
-        
+
+        input_word, target_word = np.random.choice(window,
+                                                   size=2,
+                                                   replace=False)
+
+        layer_0 = vocab[input_word]
+        layer_1 = weights_0_1[layer_0]  # Embedding Layer
+        layer_2 = layer_1.dot(weights_1_2)
+
+        target_layer = np.zeros(len(vocab))
+        target_layer[vocab[target_word]] = 10
+
+        layer_2_delta = target_layer - layer_2
+        layer_1_delta = layer_2_delta.reshape(1, -1).dot(weights_1_2.T)
+
+        weights_1_2_grad = layer_1.reshape(1, -1).T.dot(layer_2_delta.reshape(1, -1))
+        weights_0_1_grad = layer_1_delta.reshape(-1)
+
+        weights_1_2 -= alpha * weights_1_2_grad
+        weights_0_1[layer_0] -= alpha * weights_0_1_grad
+    
+    if not iteration % 10:
+        print(np.square(target_layer - layer_2).sum())
