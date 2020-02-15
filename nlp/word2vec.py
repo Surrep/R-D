6d0c12e3125edc1bfba9d4d0db84e5421a26bd99
trumanpurnell <truman.purnell@gmail.com>
2020-02-15 10:56:11 -0800

    model implemented but learns not

--- nlp/word2vec.py
+++ nlp/word2vec.py
@@ -2,10 +2,10 @@ import string
 import numpy as np
 
 # Load Data
-with open("./data/bible.txt") as fs:
+with open("/Users/trumanpurnell/Workspace/bb-labs/far/nlp/data/bible.txt") as fs:
     without_punctuation = str.maketrans('', '', string.punctuation)
 
-    corpus = fs.read().lower().translate(without_punctuation).split()
+    corpus = fs.read().lower().translate(without_punctuation).split()[:1000]
 
     vocab = {}
     identity = 0
@@ -15,25 +15,25 @@ with open("./data/bible.txt") as fs:
             identity += 1
 
 # Hyperparameters
-alpha = 0.0001
-iterations = int(1e0)
+alpha = 0.001
+iterations = int(1e3)
 window_size = 3
-embedding_size = 40
+embedding_size = 100
 
 weights_0_1_dims = (len(vocab), embedding_size)
 weights_1_2_dims = (embedding_size, len(vocab))
 
 # Weights
-weights_0_1 = np.random.randn(*weights_0_1_dims)
-weights_1_2 = np.random.randn(*weights_1_2_dims)
+weights_0_1 = np.random.randn(*weights_0_1_dims) / 100
+weights_1_2 = np.random.randn(*weights_1_2_dims) / 100
 
 # Training
-sample = corpus[:16]
+sample = corpus
 
 for iteration in range(iterations):
     for si in range(window_size, len(sample) - window_size + 1):
         window = sample[si-window_size: si+window_size]
-
+        
         input_word, target_word = np.random.choice(window,
                                                    size=2,
                                                    replace=False)
@@ -43,9 +43,9 @@ for iteration in range(iterations):
         layer_2 = layer_1.dot(weights_1_2)
 
         target_layer = np.zeros(len(vocab))
-        target_layer[vocab[target_word]] = 10
+        target_layer[[vocab[word] for word in window]] = 4
 
-        layer_2_delta = target_layer - layer_2
+        layer_2_delta = layer_2 - target_layer
         layer_1_delta = layer_2_delta.reshape(1, -1).dot(weights_1_2.T)
 
         weights_1_2_grad = layer_1.reshape(1, -1).T.dot(layer_2_delta.reshape(1, -1))
@@ -54,5 +54,4 @@ for iteration in range(iterations):
         weights_1_2 -= alpha * weights_1_2_grad
         weights_0_1[layer_0] -= alpha * weights_0_1_grad
     
-    if not iteration % 10:
-        print(np.square(target_layer - layer_2).sum())
+    print(np.square(target_layer - layer_2).sum())
