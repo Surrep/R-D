392cdd60215005171d60f9024e10354ced8a2936
trumanpurnell <truman.purnell@gmail.com>
2020-04-25 01:24:31 -0700

    adds convolutional autoencoder to main -- represent spectrograms in latent space

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
-from tools.plot import slide
 from tools.io import sndread_dir, play
 from tools.audio import real_spectrogram, real_inv_spectrogram
 
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.callbacks import TensorBoard
+
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
 # Parameters
-num_freqs = 1000
+num_freqs = 64
 sample_rate = 16000
 colors = ['r.', 'g.', 'b.', 'y.', 'k.', 'm.', 'c.']
 
 # Read data
-X = sndread_dir('./data/speech/speech_commands_v0.02/happy')
+word_0 = sndread_dir('./data/speech/speech_commands_v0.02/dog')[:5]
+word_1 = sndread_dir('./data/speech/speech_commands_v0.02/happy')[:5]
+word_2 = sndread_dir('./data/speech/speech_commands_v0.02/forward')[:5]
 
-# Spectrogram
-spectrogram = real_spectrogram(signal=X[1], num_freqs=num_freqs)
+X = np.vstack((word_0, word_1, word_2))
 
-# Display
-plt.imshow(np.abs(spectrogram), aspect='auto')
-plt.show()
+# Spectrograms
+x_train = tf.stack([
+    np.expand_dims(np.abs(real_spectrogram(word, num_freqs)), axis=-1)
+    for word in X
+])
+
+
+# Convolutions
+input_img = Input(shape=(num_freqs, sample_rate, 1))
+
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+
+autoencoder.fit(x_train, x_train,
+                epochs=200,
+                steps_per_epoch=1,
+                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
