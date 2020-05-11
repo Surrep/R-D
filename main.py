6fd81c3d6f93f35c8d49044ca0b5c89b30710b3c
trumanpurnell <truman.purnell@gmail.com>
2020-05-11 14:16:19 -0700

    embeddings now handled by LDA

--- main.py
+++ main.py
@@ -1,68 +1,75 @@
+from tools.plot import slide
 from tools.io import sndread_dir, play
-from tools.audio import real_spectrogram, real_inv_spectrogram
+from tools.audio import real_spectrogram
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
 
 # Setup
+colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',
+          'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
 words = ['dog', 'cat', 'right', 'visual', 'yes', 'wow', 'up']
-colors = ['r.', 'g.', 'b.', 'y.', 'k.', 'm.', 'c.']
 base_dir = './data/speech/speech_commands_v0.02/{}'
 
 # Parameters
 num_freqs = 64
 sample_rate = 16000
-num_files_per_word = 5
+num_files_per_word = 50
 num_classes = len(words)
+num_examples = num_classes * num_files_per_word
 
 # Read data
-x_train = tf.stack([
-    np.expand_dims(a=np.abs(real_spectrogram(word_audio, num_freqs, skip=4)),
-                   axis=-1)
-    for word_audio in np.vstack([
-        sndread_dir(base_dir.format(word), num_files_per_word)
-        for word in words
-    ])
+raw_audio = np.vstack([
+    sndread_dir(base_dir.format(word), num_files_per_word)
+    for word in words
 ])
 
-y_train = tf.keras.utils.to_categorical(
-    np.arange(num_classes * num_files_per_word) // num_files_per_word)
+x_train = tf.stack([
+    np.expand_dims(np.abs(real_spectrogram(word_audio, num_freqs)), axis=-1)
+    for word_audio in raw_audio
+])
+
+labels = np.arange(num_classes * num_files_per_word) // num_files_per_word
+y_train = tf.keras.utils.to_categorical(labels)
 
 # Construct model
 model = tf.keras.Sequential()
 
-# Input
 model.add(tf.keras.layers.Input(shape=x_train.shape[1:]))
 
-# Conv 1
+model.add(tf.keras.layers.Conv2D(16, (3, 3), padding='same'))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 32)))
+
 model.add(tf.keras.layers.Conv2D(8, (3, 3), padding='same'))
 model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 4)))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(4, 32)))
 
-# Conv 2
-model.add(tf.keras.layers.Conv2D(8, (3, 3), padding='same'))
-model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 4)))
-
-# Conv 3
-model.add(tf.keras.layers.Conv2D(8, (3, 3), padding='same'))
-model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 4)))
-
-# Conv 4
-model.add(tf.keras.layers.Conv2D(8, (3, 3), padding='same'))
-model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 4)))
-
-# Flat
 model.add(tf.keras.layers.Flatten())
-
-# Dense
 model.add(tf.keras.layers.Dense(num_classes))
-
-# Activations
 model.add(tf.keras.layers.Activation('softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy')
-model.fit(x=x_train, y=y_train, batch_size=num_files_per_word, epochs=50)
+model.fit(x=x_train, y=y_train, batch_size=num_examples, epochs=100)
+
+# Embeddings
+layer_name = 'dense_20'
+intermediate_layer = tf.keras.Model(inputs=model.input,
+                                    outputs=model.get_layer(layer_name).output)
+predictions = intermediate_layer.predict(x_train).reshape(num_examples, -1)
+
+lda = LDA(n_components=2)
+embeddings = lda.fit_transform(predictions, labels)
+
+# Plot
+for color, word in zip(colors, words):
+    plt.scatter(embeddings[labels == i, 0],
+                embeddings[labels == i, 1], c=color, label=word)
+
+plt.legend()
+plt.show()
