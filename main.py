96a99d3bd54cb8b0ced41dd4b2c8d31848b1a0b5
trumanpurnell <truman.purnell@gmail.com>
2020-05-13 01:32:27 -0700

    completes project with rhyming examples

--- main.py
+++ main.py
@@ -5,67 +5,96 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# Setup
-colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',
-          'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+# Utils
 
-# words = ['backward', 'bird', 'marvin', 'happy', 'cat', 'house']
-words = ['backward', 'bed', 'bird', 'cat', 'dog',
-         'down', 'eight', 'five', 'follow', 'forward',
-         'four', 'go', 'happy', 'house', 'learn', 'left',
-         'nine', 'no', 'off', 'on', 'one', 'right', 'marvin',
-         'seven', 'sheila', 'six', 'stop', 'three', 'tree',
-         'two', 'up',  'visual', 'wow', 'yes', 'zero']
+
+def show(title):
+    plt.title(title)
+    plt.legend()
+    plt.show()
+
+
+def sample(items, size):
+    sample_range = range(len(items))
+
+    return [(items[i], i) for i in np.random.choice(sample_range, size, False)]
+
+
+# Setup
+words = SpeechCommands.all_words
+
+rhymes = [('forward', 9), ('backward', 0), ('tree', 28),
+          ('sheila', 24), ('three', 27), ('go', 11), ('no', 17)]
+
+colors = ['tab:blue', 'tab:orange', 'tab:green',
+          'tab:red', 'tab:purple', 'tab:brown', 'tab:pink']
 
 # Parameters
-num_files = 3
+samples = 4
+dim_embed = 2
+num_files = 1000
 num_classes = len(words)
 num_examples = num_classes * num_files
 labels = np.arange(num_examples) // num_files
 
-# Training data
-x_train = SpeechCommands.load_spec(words, num_files)
+
+# Training Data
+x_raw = SpeechCommands.load_raw(words, num_files)
+x_train = tf.expand_dims(SpeechCommands.load_spec(words, num_files), -1)
 y_train = tf.keras.utils.to_categorical(labels)
 
-x_val = SpeechCommands.load_spec(words)
-
-# Construct model
+# Model
 model = tf.keras.Sequential()
 
 model.add(tf.keras.layers.Input(shape=x_train.shape[1:]))
 
-model.add(tf.keras.layers.Conv2D(4, (3, 3), padding='same'))
-model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(1, 32)))
-
-model.add(tf.keras.layers.Conv2D(4, (3, 3), padding='same'))
-model.add(tf.keras.layers.Activation('relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(4, 32)))
-
-model.add(tf.keras.layers.Conv2D(4, (1, 1), padding='same'))
+model.add(tf.keras.layers.Conv2D(32, (3, 3), padding='same'))
 model.add(tf.keras.layers.Activation('relu'))
 
-model.add(tf.keras.layers.Conv2D(4, (1, 1), padding='same'))
+model.add(tf.keras.layers.Conv2D(32, (3, 3)))
 model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+
+model.add(tf.keras.layers.Conv2D(64, (3, 3), padding='same'))
+model.add(tf.keras.layers.Activation('relu'))
+
+model.add(tf.keras.layers.Conv2D(64, (3, 3)))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
 
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(num_classes))
 model.add(tf.keras.layers.Activation('softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy')
-# model.fit(x=x_train, y=y_train, batch_size=20, epochs=100)
+model.fit(x=x_train, y=y_train, batch_size=100, epochs=8)
 
-# Embeddings
-# dense = tf.keras.Model(inputs=model.input, outputs=model.layers[-2].output)
-# predictions = dense.predict(x_train).reshape(num_examples, -1)
+# Convolutional LDA Plot
+layer_model = tf.keras.Model(inputs=model.input,
+                             outputs=model.layers[-2].output)  # dense
 
-# lda = LDA(n_components=2)
-# embeddings = lda.fit_transform(predictions, labels)
+predictions = layer_model.predict(x_train).reshape(num_examples, -1)
 
-# Plot
-# for i, (color, word) in enumerate(zip(colors, words)):
-#     plt.scatter(embeddings[labels == i, 0],
-#                 embeddings[labels == i, 1], c=color, label=word)
+lda = LDA(n_components=dim_embed)
+embeddings = lda.fit_transform(predictions, labels)
 
-# plt.legend()
-# plt.show()
+for color, (word, i) in zip(colors, rhymes):
+    plt.scatter(embeddings[labels == i, 0],
+                embeddings[labels == i, 1], c=color, label=word)
+
+show('Convolutional LDA as applied to rhyming words')
+
+# Raw LDA Plot
+raw_clips = np.vstack([x_raw[labels == i] for _, i in rhymes])
+raw_labels = np.vstack([labels[labels == i] for _, i in rhymes]).ravel()
+
+lda = LDA(n_components=dim_embed)
+raw_embeddings = lda.fit_transform(raw_clips, raw_labels)
+
+for color, (word, i) in zip(colors, rhymes):
+    plt.scatter(raw_embeddings[raw_labels == i, 0],
+                raw_embeddings[raw_labels == i, 1], c=color, label=word)
+
+show('Conventional LDA as applied to rhyming words')
